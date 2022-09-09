35aab985989d8e6d092a053855b98008a1a050d0
emmersontay <109201424+emmersontay@users.noreply.github.com>
2022-09-09 21:07:29 +1000

    Week 7 Tasks

---
+++ week7.js
@@ -0,0 +1,34 @@
+Vue.component('component1', {
+    template: '<div>{{ message }}</div>',
+    data: function () {
+        return {
+            message: 'This is my component 1'
+        }
+    }
+});
+Vue.component('component 2', {
+    template: '<div>This is component 2</div>',
+});
+Vue.component('global', {
+    template: '<div>This is global</div>',
+});
+var component1 = {
+    template: '<div><global></global>This is component 1 in a global</div>',
+};
+var component2 = {
+    template: '<div><component1></component1>This is component 2 in component 1</div>',
+    component:{
+        component1,
+    }
+};
+var app = new Vue ({
+    el: '#app',
+    data: {
+        text: 'These are my local and global components.',
+
+    },
+    components: {
+        component1,
+        component2,
+    },
+})
